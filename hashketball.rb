fd638afff851094348e99da543b46fe10ccb9ec4
Danielle Matton <danimatton@gmail.com>
2019-11-11 17:35:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -236,7 +236,7 @@ def winning_team
   game_hash.each do |place, team|
     team_points = 0
     team_name = game_hash[place][:team_name]
-    team.each do |score|
+    team[]
       score.sum |v|
     end
   end
