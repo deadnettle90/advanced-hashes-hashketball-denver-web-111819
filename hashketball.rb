590923d00d224b986c37788ec7407c2bea3f9726
Danielle Matton <danimatton@gmail.com>
2019-11-11 18:48:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -249,5 +249,8 @@ def player_with_longest_name
       longest_name << player[:player_name]
     end
   end
-  longest_name.max_by {|name| name.length}
+  longest_name.max_by do
+    |name| name.length
+    
+  end
 end
