135030ff571e5ba52dab507ea0e2713da5514ccc
Danielle Matton <danimatton@gmail.com>
2019-11-11 18:31:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -231,17 +231,16 @@ def most_points_scored
 end
 
 def winning_team
-   top_team = {}
+   winner = {}
   game_hash.values.each do |team_info|
     team_points = 0
     team_info[:players].each do |player|
       team_points += player[:points]
     end
-    top_team[team_info[:team_name]] = team_points
+    winner[team_info[:team_name]] = team_points
   end
-  top_team.key(top_team.values.max) 
+  winner.key(winner.values.max) 
 end
-
 
 def player_with_longest_name
   
